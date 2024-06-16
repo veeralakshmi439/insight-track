bb54b8bdd98477a3dbe399ba90089cdf7834dde5
krishnasagar <sagar474@hotmail.com>
2024-06-16 17:55:35 +0100

    add

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,13 +4,10 @@ import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import AuthProvider from "./Login/AuthProvider.tsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </AuthProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
