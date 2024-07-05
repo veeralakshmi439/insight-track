3625501fec9fa34a63e6bbe216d4104cee7017df
krishnasagar <sagar474@hotmail.com>
2024-07-05 12:44:06 +0100

    add previous screenshot time stamp logic

--- collectors/synthetic-scans/TimerTrigger1/runbrowser.js
+++ collectors/synthetic-scans/TimerTrigger1/runbrowser.js
@@ -105,7 +105,7 @@ const runBrowser = async (context, applicationPage) => {
           harfile
         },
         previousScan: {
-          screenshotPath: blobUrl,
+          screenshotPath: blobUrl.replace(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/g, applicationPage.timestamp),
           harfile
         },
       })
